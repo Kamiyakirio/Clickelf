42713e7e1ca3ee1b293b6120bc8e468be970f76d
KamiyaKirio <cindy@SHIVA>
2024-10-15 14:31:59 +0800

    ver 2

---
+++ ClickElf/Keymap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickElf
+{
+    internal class Keymap
+    {
+        [DllImport("user32.dll")]
+        private static extern uint MapVirtualKey(uint uCode, uint uMapType);
+        private const uint MAPVK_VK_TO_CHAR = 2;
+
+        public Keymap()
+        {
+        }
+    }
+}
