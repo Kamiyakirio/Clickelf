50cb8ebe2efb24b1ded9c2ceb4fa4ee23ac07577
KamiyaKirio <cindy@SHIVA>
2025-02-18 17:51:29 +0800

    again added

---
+++ ClickElf/FormExecute.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ClickElf
+{
+    partial class FormExecute
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textTimes = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSwitchForm = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(86, 43);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择脚本";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(104, 27);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(41, 12);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "重复次数";
+            // 
+            // textTimes
+            // 
+            this.textTimes.Location = new System.Drawing.Point(71, 64);
+            this.textTimes.Name = "textTimes";
+            this.textTimes.Size = new System.Drawing.Size(182, 21);
+            this.textTimes.TabIndex = 3;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 91);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(111, 45);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "执行";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(170, 107);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(41, 12);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "未执行";
+            // 
+            // btnSwitchForm
+            // 
+            this.btnSwitchForm.Location = new System.Drawing.Point(289, 12);
+            this.btnSwitchForm.Name = "btnSwitchForm";
+            this.btnSwitchForm.Size = new System.Drawing.Size(102, 43);
+            this.btnSwitchForm.TabIndex = 6;
+            this.btnSwitchForm.Text = "更多功能";
+            this.btnSwitchForm.UseVisualStyleBackColor = true;
+            this.btnSwitchForm.Click += new System.EventHandler(this.btnSwitchForm_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(280, 91);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(111, 45);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FormExecute
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 155);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnSwitchForm);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.textTimes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "FormExecute";
+            this.Text = "FormExecute";
+            this.Load += new System.EventHandler(this.FormExecute_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textTimes;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSwitchForm;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ ClickElf/Mouse/MouseTracker.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+public class MouseTracker
+{
+    // 定义钩子委托
+    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    // DLL导入
+    [DllImport("user32.dll")]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    private const int WH_MOUSE_LL = 14;
+    private const int WM_MOUSEMOVE = 0x0200;
+    private const int WM_LBUTTONDOWN = 0x0201;
+    private const int WM_LBUTTONUP = 0x0202;
+    private const int WM_RBUTTONDOWN = 0x0207;
+    private const int WM_RBUTTONUP = 0x0208;
+
+    private LowLevelMouseProc _proc;
+    private IntPtr _hookID = IntPtr.Zero;
+
+    public event Action<int, int> MouseMoved; // 定义鼠标移动事件
+    public event Action<Point, bool, int> MousePress;
+
+    public MouseTracker()
+    {
+        _proc = HookCallback;
+        _hookID = SetHook(_proc);
+    }
+
+    ~MouseTracker()
+    {
+        UnhookMouse();
+    }
+
+    private IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (var curProcess = Process.GetCurrentProcess())
+        using (var curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private void UnhookMouse()
+    {
+        UnhookWindowsHookEx(_hookID);
+    }
+
+    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_MOUSEMOVE)
+        {
+            var mouseHookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+            MouseMoved?.Invoke(mouseHookStruct.pt.x, mouseHookStruct.pt.y); // 触发事件
+        }
+        if (nCode >= 0)
+        {
+            var mouseHookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+            var position = new Point(mouseHookStruct.pt.x, mouseHookStruct.pt.y);
+
+            switch ((int)wParam)
+            {
+                case WM_LBUTTONDOWN:
+                    MousePress?.Invoke(position, true, 0);
+                    break;
+                case WM_LBUTTONUP:
+                    MousePress?.Invoke(position, false, 0);
+                    break;
+                case WM_RBUTTONDOWN:
+                    MousePress?.Invoke(position, true, 1);
+                    break;
+                case WM_RBUTTONUP:
+                    MousePress?.Invoke(position, false, 1);
+                    break;
+            }
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    public void Dispose()
+    {
+        UnhookMouse();
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+}
